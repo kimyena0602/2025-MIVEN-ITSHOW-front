6f309cff69ea17988c1e56280d88d3f6d3060efb
연아윤 <w2306@e-mirim.hs.kr>
2025-05-13 01:01:19 +0900

    feat. selectbook 책 위치 조정

--- src/pages/SelectBook.jsx
+++ src/pages/SelectBook.jsx
@@ -1,1 +1,34 @@
-// newPage or 이미 작성한 책 리뷰
+import React from "react";
+import Nav from "../components/Nav";
+import styles from "../styles/selectbook.module.css";
+
+// 이미지 import
+import img1 from "../assets/images/selectbook1.png";
+import img2 from "../assets/images/selectbook2.png";
+import img3 from "../assets/images/selectbook3.png";
+import img7 from "../assets/images/newpage.png";
+import img4 from "../assets/images/selectbook4.png";
+import img5 from "../assets/images/selectbook5.png";
+import img6 from "../assets/images/selectbook6.png";
+
+export default function SelectBook() {
+  return (
+    <div className={styles.container}>
+      <Nav />
+      <div className={styles.gallery}>
+        <img src={img1} className={`${styles.image} ${styles.sizeXS}`} alt="book1" />
+        <img src={img2} className={`${styles.image} ${styles.sizeS}`} alt="book2" />
+        <img src={img3} className={`${styles.image} ${styles.sizeM}`} alt="book3" />
+        
+        <div className={styles.newPage}>
+          <div className={styles.plus}>＋</div>
+          <div className={styles.text}>new<br />page</div>
+        </div>
+
+        <img src={img4} className={`${styles.image} ${styles.sizeM}`} alt="book4" />
+        <img src={img5} className={`${styles.image} ${styles.sizeS}`} alt="book5" />
+        <img src={img6} className={`${styles.image} ${styles.sizeXS}`} alt="book6" />
+      </div>
+    </div>
+  );
+}
